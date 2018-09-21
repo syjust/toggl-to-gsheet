fd7b89a5a07839b14cb0eb5eacd82ceb2c14095e
Sylvain JUST <sylvain.just@i-sloth.com>
2018-09-21 14:12:42 +0000

    extract the main action from gapi to app.js
    branch:master

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-require('./google-api.js');
+const GoogleSheetApi = require('./google-api.js');
+gApi = new GoogleSheetApi();
+gApi.listMajors(printMajors);
+
+function printMajors(res) {
+  var rows = res.data.values;
+  if (rows.length) {
+    console.log('Name, Major:');
+    // Print columns A and E, which correspond to indices 0 and 4.
+    rows.map((row) => {
+      console.log(`${row[0]}, ${row[4]}`);
+    });
+  } else {
+    console.log('No data found.');
+  }
+}
--- google-api.js
+++ google-api.js
@@ -3,30 +3,58 @@ const readline = require('readline');
 const {google} = require('googleapis');
 const GoogleAuth = require('./google-auth.js');
 
-gAuth = new GoogleAuth();
-gAuth.loadGoogleApi(listMajors);
+module.exports = class GoogleSheetApi {
 
-/**
- * Prints the names and majors of students in a sample spreadsheet:
- * @see https://docs.google.com/spreadsheets/d/1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms/edit
- * @param {google.auth.OAuth2} auth The authenticated Google OAuth client.
- */
-function listMajors(auth) {
-  const sheets = google.sheets({version: 'v4', auth});
-  sheets.spreadsheets.values.get({
-    spreadsheetId: '1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms',
-    range: 'Class Data!A2:E',
-  }, (err, res) => {
-    if (err) return console.log('The API returned an error: ' + err);
-    const rows = res.data.values;
-    if (rows.length) {
-      console.log('Name, Major:');
-      // Print columns A and E, which correspond to indices 0 and 4.
-      rows.map((row) => {
-        console.log(`${row[0]}, ${row[4]}`);
+  /**
+   * @param {callbacks} map with action to do for each functions
+   */
+  constructor(callbacks = {}) {
+    this.gAuth = new GoogleAuth();
+    this.callbacks = callbacks ;
+
+    this.initBoundedFunctions();
+  }
+
+  /**
+   * @function initBoundedFunctions
+   *
+   * define here functions that need to be used as callback with this references inside them
+   */
+  initBoundedFunctions() {
+    var self = this;
+
+    /**
+     * @function getMajors
+     *
+     * Get the names and majors of students in a sample spreadsheet:
+     *
+     * @param {google.auth.OAuth2} auth The authenticated Google OAuth client.
+     * @see https://docs.google.com/spreadsheets/d/1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms/edit
+     */
+    this.getMajors = function(auth) {
+      const sheets = google.sheets({version: 'v4', auth});
+      sheets.spreadsheets.values.get({
+        spreadsheetId: '1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms',
+        range: 'Class Data!A2:E',
+      }, (err, res) => {
+        if (err) return console.log('The API returned an error: ' + err);
+        if (self.callbacks.getMajors !== undefined) {
+          self.callbacks.getMajors(res);
+        }
       });
-    } else {
-      console.log('No data found.');
     }
-  });
+  }
+
+  /**
+   * @function listMajors
+   *
+   * @param callback
+   */
+  listMajors(callback = undefined) {
+    if (callback !== undefined) {
+      this.callbacks.getMajors = callback;
+    }
+    this.gAuth.loadGoogleApi(this.getMajors);
+  }
+
 }
