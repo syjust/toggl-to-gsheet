19fa5f8493564e26ccc35d718bffb0e48f5ae1a8
Sylvain JUST <sylvain.just@i-sloth.com>
2018-09-24 15:21:59 +0000

    gapi updateSheet first static impl
    branch:master

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const GoogleSheetApi = require(__dirname+'/src/google-api.js');
 
 gApi = new GoogleSheetApi();
-gApi.listMajors(printMajors);
+//gApi.listMajors(printMajors);
+gApi.updateSheet(printSheet);
 
 function printMajors(res) {
   var rows = res.data.values;
@@ -10,6 +11,18 @@ function printMajors(res) {
     // Print columns A and E, which correspond to indices 0 and 4.
     rows.map((row) => {
       console.log(`${row[0]}, ${row[4]}`);
+    });
+  } else {
+    console.log('No data found.');
+  }
+}
+function printSheet(res) {
+  var rows = res.data.values;
+  if (rows.length) {
+    console.log('sheet');
+    // Print columns A and E, which correspond to indices 0 and 4.
+    rows.map((row) => {
+      console.log(JSON.stringify(row));
     });
   } else {
     console.log('No data found.');
--- src/google-api.js
+++ src/google-api.js
@@ -35,7 +35,35 @@ module.exports = class GoogleSheetApi {
       const sheets = google.sheets({version: 'v4', auth});
       sheets.spreadsheets.values.get({
         spreadsheetId: '1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms',
-        range: 'Class Data!A2:E',
+        range: 'Class Data!A2:E'
+      }, (err, res) => {
+        if (err) return console.log('The API returned an error: ' + err);
+        if (self.callbacks.getMajors !== undefined) {
+          self.callbacks.getMajors(res);
+        }
+      });
+    }
+
+    this.setSheet = function(auth) {
+      var good_sheet    = "1DWRHTx-i4_khB4A6nja54JJ02hPgcXMUY9EsG_d20x4/edit#gid=1703643935";
+      var default_sheet = "1DWRHTx-i4_khB4A6nja54JJ02hPgcXMUY9EsG_d20x4";
+      const sheets = google.sheets({version: 'v4', auth});
+      sheets.spreadsheets.values.update({
+        spreadsheetId: default_sheet,
+        range: 'Toggl_time_entries_2018-01-01_to_2018-12-31!A:N',
+        valueInputOption: 'RAW',
+        resource: {
+            "range": 'Toggl_time_entries_2018-01-01_to_2018-12-31!A:N',
+            "majorDimension": 'ROWS',
+            "values": [
+              [ 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N' ],
+              [ 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N' ],
+              [ 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N' ],
+              [ 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N' ],
+              [ 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N' ],
+              [ 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N' ],
+            ]
+        }
       }, (err, res) => {
         if (err) return console.log('The API returned an error: ' + err);
         if (self.callbacks.getMajors !== undefined) {
@@ -55,6 +83,18 @@ module.exports = class GoogleSheetApi {
       this.callbacks.getMajors = callback;
     }
     this.gAuth.loadGoogleApi(this.getMajors);
+  }
+
+  /**
+   * @function updateSheet
+   *
+   * @param callback
+   */
+  updateSheet(callback = undefined) {
+    if (callback !== undefined) {
+      this.callbacks.setSheet = callback;
+    }
+    this.gAuth.loadGoogleApi(this.setSheet);
   }
 
 }
