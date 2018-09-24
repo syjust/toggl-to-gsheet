1294e7483f5f0226d47f1b5845ebca7c88037785
Sylvain JUST <sylvain.just@i-sloth.com>
2018-09-24 14:42:48 +0000

    extract year ttt form toggl-report script
    branch:master

--- toggl-report.sh
+++ toggl-report.sh
@@ -45,23 +45,7 @@ fi
 # }}}
 
 # {{{ loading parameters : only 'year' for moment
-if [ -z "$1" ] ; then
-  # auto year
-  year=`date "+%Y"`
-  month=`date "+%m"`
-  day=`date "+%d"`
-  if [ "x$month" == "x01" -a "x$day" == "x01" ] ; then
-    # taking last year please
-    let year--
-  fi
-else
-  # manual year
-  year="$1"
-  shift
-  if ! [[ $year =~ ^2[0-9]{3}$ ]] ; then
-    quit "'$year' : YEAR is not well formatted"
-  fi
-fi
+source inc/load-year.inc.sh
 # }}}
 
 since="$year-01-01"
