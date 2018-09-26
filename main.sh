83b33d29bb94e31d14a96acdbbcebce4b2699c3d
Sylvain JUST <sylvain.just@i-sloth.com>
2018-09-26 14:09:24 +0000

    update main.sh to take node arguments
    branch:master

--- main.sh
+++ main.sh
@@ -3,11 +3,12 @@
 # {{{ function usage
 #
 usage() {
-  color_and_prefix _cyan "$0 [YEAR]"
+  color_and_prefix _cyan "$0 SPREADSHEETID [YEAR]"
   info
   info "This script manage TOGGL & GOOGLE scripts to work with YEAR (if given, else, the current year is used)"
   info "TOGGL script retrieve toggl inputs for YEAR given"
   info "GOOGLE script send toggle-reports-YEAR.csv into YEAR tab of configured spreadsheet"
+  info "tab Toggl_time_entries_YEAR-01-01_to_YEAR-12-31 must exists in target spreadsheet given as argument with SPREADSHEETID"
 }
 export -f usage
 # }}}
@@ -21,19 +22,21 @@ else
 fi
 # }}}
 
-# {{{ loading parameters : only 'year' for moment
+# {{{ loading parameters : 'YEAR' and 'SPREADSHEETID' as parameter
+# @param $1 SPREADSHEETID
+# @param $2 [YEAR]
+[ -z $1 ] && quit "I need SPREADSHEETID as first argument"
+spreadsheetId=$1 ; shift
 source inc/load-year.inc.sh
 # }}}
 
-TOGGL="toggl-report.sh"
+TOGGL="./toggl-report.sh"
 GOOGLE="app.js"
 
 for script in TOGGL GOOGLE ; do
   [ -f "${!script}" ] || quit "${!script} : $script script not found"
 done
 
-# generate toogle-reports.csv script
-$TOGGL $year
-
-# send csv to google sheet
-node $GOOGLE toggle-reports-$year.csv
+# generate toogle-reports.csv script && send it to google spreadsheet
+$TOGGL $year \
+  && node $GOOGLE -c $spreadsheetId -s $year
